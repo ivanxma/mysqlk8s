39c70f0ab95ffb6241529536e42539c89e300d75
Oracle Public Cloud User <opc@ivanma-demo1.sub07160345580.mysqlvcnivanma.oraclevcn.com>
2021-04-27 03:25:10 +0000

    Initial

---
+++ 01-secret.sh
@@ -0,0 +1,4 @@
+
+ kubectl create namespace mysql-operator
+
+ kubectl create secret docker-registry 'ivanma-secret-mysql-operator' -n mysql-operator --docker-server=iad.ocir.io --docker-username='idazzjlcjqzj/oracleidentitycloudservice/ivan-cs.ma@oracle.com' --docker-password='E5MI3#i5OhF8Kuje]}:;'
---
+++ cluster/01-create-secret.sh
@@ -0,0 +1,2 @@
+kubectl create -n mysql-operator secret generic mypwds --from-literal=rootUser=myroot  --from-literal=rootHost=%  --from-literal=rootPassword=Welcome1!
+
