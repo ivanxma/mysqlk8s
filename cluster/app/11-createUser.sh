b108fc0d4e44b20e6e1e1b3133a3372576e8c5c2
Oracle Public Cloud User <opc@ivanma-demo1.sub07160345580.mysqlvcnivanma.oraclevcn.com>
2021-04-29 14:45:02 +0000

    make change on creating user node

--- cluster/app/11-createUser.sh
+++ cluster/app/11-createUser.sh
@@ -1,5 +1,6 @@
 # create user node'%' identified by 'pass';
 # grant all on *.* to node@'%';
-echo "ls" | kubectl exec -it -n $1 mycluster-0  -- bash
+ kubectl exec -it -n $1 mycluster-0  -- mysqlsh --sql myroot:Welcome1!@localhost -e "create user if not exists node@'%' identified by 'pass'; grant all on *.* to node@'%';"
+
 
 
