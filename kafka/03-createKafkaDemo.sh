4f58765ae6099cbbbe5e2393d6fd5dc1d8d03550
Ivan Ma <ivanxma@yahoo.com>
2022-03-07 10:13:33 +0800

    update pvc

--- kafka/03-createKafkaDemo.sh
+++ kafka/03-createKafkaDemo.sh
@@ -1,1 +1,3 @@
+kubectl apply -f pvc.yaml -n kafka
+kubectl get pvc kafka
 helm install kafka-demo --namespace kafka incubator/kafka -f https://raw.githubusercontent.com/helm/charts/master/incubator/kafka/values.yaml
