823783383912fe1595f30207d79c7e5869db2b8c
ivanxma <ivanxma@yahoo.com>
2024-03-07 16:47:53 +0800

    update

--- jupyter/jupyterhub/01-addRepo.sh
+++ jupyter/jupyterhub/01-addRepo.sh
@@ -1,3 +1,5 @@
 helm repo add jupyterhub https://hub.jupyter.org/helm-chart/
+helm repo add bitnami https://charts.bitnami.com/bitnami                        
+
 helm repo update
 helm search repo jupyterhub
